1e77a3476997ee2da8adf51d4f6f7ec572752a95
Amr Gamal <113727947+AmrrGamal@users.noreply.github.com>
2023-04-28 11:30:16 +0200

    updated

--- Views/Users/selectAcct.php
+++ Views/Users/selectAcct.php
@@ -1,3 +1,19 @@
+
+
+<?php
+require_once '../../Controllers/AccountController.php';
+$account = new AccountController;
+$result = $account->selectAllAccounts();
+
+if(isset($_POST["selectAccount"])){
+   $id=$_POST["accId"];
+   $type=$_POST["accType"];
+   $_SESSION["accId"]=$id;
+   $_SESSION["accType"]=$type;
+   header('location:transactions.php');
+   
+}
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -14,17 +30,30 @@
         <h1 style="text-align: center">Select Account</h1>
     </div>
 
-    <div class="row">
-        <div class="col-md-2"></div>
-        <div class="col-md-8"><a href="transactions.php"  class="btn btn-primary btn-lg accountButton">The current account: 12345678911</a></div>
-        <div class="col-md-2"></div>
-    </div>
+<?php
+foreach($result as $res){
+?>
 
-    <div class="row">
+<div class="row">
+    <form  method="POST">
+
+        <input type="hidden" name="accId" value="<?php echo $res["accountId"];?>">
+        <input type="hidden" name="accType" value="<?php echo $res["accountType"];?>">
+
         <div class="col-md-2"></div>
-        <div class="col-md-8"><a href="transactions.php" class="btn btn-primary btn-lg accountButton">The saving account: 11987654321</a></div>
+        <div class="col-md-8"> <button type="submit" name="selectAccount" class="btn btn-primary btn-lg accountButton">
+        <?php echo $res["accountType"] .' : '. $res["accountId"]; ?> 
+        </button> </div>
         <div class="col-md-2"></div>
-    </div>
+
+    </form>
+</div>  
+    
+<?php
+}
+?>
+
+    
 
     <div class="row">
         <div class="col-sm-4"></div>
@@ -32,6 +61,8 @@
         <div class="col-sm-4"><a href="../Auth/index.php"  class="btn btn-danger btn-lg returnCardButton">Back</a></div>
     </div>
 </div>
+
+
 
 
 </body>
